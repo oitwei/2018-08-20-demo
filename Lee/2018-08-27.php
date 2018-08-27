d58043b19bfa827163478a177bcd3c94c8ba05c3
Lee <afk951106@gmail.com>
2018-08-27 19:50:38 +0800

    add 2018-08-27

---
+++ Lee/2018-08-27.php
@@ -0,0 +1,8 @@
+<body>
+<form action="2018-08-27-2.php" method="post">
+Account:<input type="text" name="account"><br>
+Passsword:<input type="text" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
