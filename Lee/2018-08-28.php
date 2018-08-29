77a52c062bb0b2a68ee22ef52ef18bf0a64f7ae4
Lee <afk951106@gmail.com>
2018-08-29 13:34:11 +0800

    2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest 2018-08-28formtest

---
+++ Lee/2018-08-28.php
@@ -0,0 +1,15 @@
+<body>
+GET
+<form action="2018-08-28-2.php" method="get">
+Account:<input type="text" name="account"><br>
+Passsword:<input type="password" name="password"><br>
+<input type="submit" value="執行">
+</form>
+
+POST 
+<form action="2018-08-28-2.php" method="post">
+Account:<input type="text" name="account_P" ><br>
+Passsword:<input type="password" name="password_p"><br>
+<input type="submit" value="執行">
+
+</body>
