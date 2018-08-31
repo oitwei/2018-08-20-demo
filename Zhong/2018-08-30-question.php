c3df9d0febbfa8ac6b885bb4b82cdc89ce94caa6
chzhonge <chzhonge@gmail.com>
2018-09-01 02:09:55 +0800

    update 2018-08-30-question.php for use array_column

--- Zhong/2018-08-30-question.php
+++ Zhong/2018-08-30-question.php
@@ -1,39 +1,26 @@
 <?php
 
 $file = fopen(__DIR__ . '/2018-08-30-question.txt', 'r');
-
 $handle = $file;
 
-$current_line = 1;
-$column_name = [];
+$columns_name = [];
+$csv_data = [];
+
 $output = [];
-$mapping = [];
+$current_line = 1;
 
 while (($buffer = fgets($handle)) !== false) {
     if (1 === $current_line) {
-        $column_name = explode(',', $buffer);
-        $column_data = [];
-
-        foreach ($column_name as $index => $name) {
-            $column_data[$name] = [];
-            $mapping[$index] = $name;
-        }
-    } else {
-        // list($name, $age, $weight) =
-        $result = explode(',', $buffer);
-        foreach ($result as $index => $row) {
-            $column_data[$mapping[$index]][] = $row;
-        }
+        $columns_name = explode(',', $buffer);
+        $current_line += 1;
+        continue;
     }
-    $current_line ++;
+
+    $csv_data[] = explode(',', $buffer);
 }
 
-
-
-// $people_count = count($output);
-// $people_average_age = array_sum(array_column($output, 'age')) / $people_count;
-// $people_average_weight = array_sum(array_column($output, 'weight')) / $people_count;
-
-// var_dump($people_count, $people_average_age, $people_average_weight);
+foreach ($columns_name as $index => $name) {
+    $output[$name] = array_column($csv_data, $index);
+}
 
 fclose($handle);
