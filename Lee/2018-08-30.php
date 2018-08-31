936a00dd7dc5a63e47cdbe2518b957ffb02ef441
Lee <afk951106@gmail.com>
2018-08-31 23:51:47 +0800

    midufied

--- Lee/2018-08-30.php
+++ Lee/2018-08-30.php
@@ -1,25 +1,36 @@
 <?php
 $file = fopen('2018-08-30-question.txt', "r");
 $array = [];
+$array2 = [];
+$agesum = 0;
+$ageaverage = 0;
+$weightsum = 0;
+$weightaverage = 0;
+$i = 0;
 while (($buffer = fgets($file)) !== false) {
-    
     $array[] = $buffer;   
     
+}
+
+
+foreach ($array as $value) { 
     
+    if ($i>0) {
+        $array2[] = explode(',', $value);
+    }
+    $i++;
 }
 
-
-foreach ($array as $value) {
-    $value =
-
+foreach ($array2 as $value) {
+    $agesum = intval($value[1]) + $agesum;
+    $weightsum = intval($value[2]) + $weightsum;
 }
-var_dump($array);
-$usercount = (count($array))-1;
-echo '人數:' . $usercount ;
-echo '平均分數' ;
 
-
-
-
+$ageaverage = intval($agesum/(count($array)-1));
+$weightaverage = intval($weightsum/(count($array)-1));
+$usercount = (count($array)-1);
+echo '人數:' . $usercount;
+echo '平均年紀' . $ageaverage;
+echo '平均體重'. $weightaverage ;
 fclose($file)
 ?>
