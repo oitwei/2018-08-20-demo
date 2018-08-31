6312191e90deb89a5e2a5351a3f4c46f55996c86
Lee <afk951106@gmail.com>
2018-09-01 01:12:07 +0800

    wq

--- Lee/2018-08-30.php
+++ Lee/2018-08-30.php
@@ -15,15 +15,23 @@ while (($buffer = fgets($file)) !== false) {
 
 
 foreach ($array as $value) { 
+    if ($i === 0) {
+        $array3=explode(',', $value);
+
+    }
     if ($i > 0) {
         $array2[] = explode(',', $value);
     }
+    
     $i++;
 }
-
 foreach ($array2 as $value) {
-    $agesum = intval($value[1]) + $agesum;
-    $weightsum = intval($value[2]) + $weightsum;
+    $array3['姓名'] = $value[0];
+    $array3['年紀'] = $value[1];
+    $array3['體重'] = $value[2];
+    $agesum = intval($array3['年紀']) + $agesum;
+    $weightsum = intval($array3['體重']) + $weightsum;
+
 }
 
 $ageaverage = intval($agesum/(count($array)-1));
