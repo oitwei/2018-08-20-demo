12fc8de368b82ad86e620f93988219acf9b52b4f
Lee <afk951106@gmail.com>
2018-09-03 17:49:28 +0800

    nochange

--- Zhong/2018-08-30-question.php
+++ Zhong/2018-08-30-question.php
@@ -13,10 +13,12 @@ while (($buffer = fgets($handle)) !== false) {
     if (1 === $current_line) {
         $column_name = explode(',', $buffer);
         $column_data = [];
+        
 
         foreach ($column_name as $index => $name) {
             $column_data[$name] = [];
             $mapping[$index] = $name;
+           
         }
     } else {
         // list($name, $age, $weight) =
@@ -30,10 +32,10 @@ while (($buffer = fgets($handle)) !== false) {
 
 
 
-// $people_count = count($output);
-// $people_average_age = array_sum(array_column($output, 'age')) / $people_count;
-// $people_average_weight = array_sum(array_column($output, 'weight')) / $people_count;
+$people_count = count($output);
+$people_average_age = array_sum(array_column($output, 'age')) / $people_count;
+$people_average_weight = array_sum(array_column($output, 'weight')) / $people_count;
 
-// var_dump($people_count, $people_average_age, $people_average_weight);
+var_dump($people_count, $people_average_age, $people_average_weight);
 
 fclose($handle);
