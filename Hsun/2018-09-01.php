667b3b2c931d2945cd03f39534149dd15ceb52f2
peter <peterhsun13@gmail.com>
2018-09-06 21:51:03 +0800

    "2018-09-06"

--- Hsun/2018-09-01.php
+++ Hsun/2018-09-01.php
@@ -1,57 +1,56 @@
 <?php
 
 $file = fopen(__DIR__ . '/../Zhong/2018-08-30-question.txt', 'r');
-$handle = $file;
-$arrayData = [];
-while (($buffer = fgets($handle)) !== false) {
-    $arrayData[] = trim($buffer);
+$usersData = [];
+while (($buffer = fgets($file)) !== false) {
+    $usersData[] = trim($buffer);
 }
-fclose($handle);
-$arrayTitle = $arrayData[0];
-unset($arrayData[0]); 
-$arrayTitle = explode(",", $arrayTitle);
+fclose($file);
+$usersDataTitle = $usersData[0];
+unset($usersData[0]); 
+$usersDataTitle = explode(',', $usersDataTitle);
 
-foreach ($arrayData as $value) {
-    $arrayValue[] = explode(",", $value);
-}
-
-foreach ($arrayTitle as $index => $title) {
-    $arrayTitleIndex[$title] = [];
-    $arrayNumberIndex[$index] = $title;
+foreach ($usersData as $value) {
+    $usersDataValue[] = explode(',', $value);
 }
 
-foreach ($arrayValue as $value) {
-    foreach ($value as $index => $columnValueOfValue) {
-        $key = $arrayNumberIndex[$index];
-        $arrayTitleIndex[$key][] = $columnValueOfValue;
+foreach ($usersDataTitle as $index => $title) {
+    $usersDataTitleInIndex[$title] = [];
+}
+
+foreach ($usersDataValue as $arrayUsersValue) {
+    foreach ($arrayUsersValue as $index => $value) {
+        $key = $usersDataTitle[$index];
+        $usersDataTitleInIndex[$key][] = $value;
     }
 }
+
 $i = 0;
 $weightTemp = 0;
 $ageSum = 0;
 $weightSum = 0;
 $ageTemp = 0;
 
-foreach ($arrayValue as $value) {
-    $ageTemp = intval($arrayTitleIndex['age'][$i]);
+foreach ($usersDataValue as $value) {
+    $ageTemp = intval($usersDataTitleInIndex['age'][$i]);
     $ageSum = $ageSum + $ageTemp;
-    $weightTemp = intval($arrayTitleIndex['weight'][$i]);
+    $weightTemp = intval($usersDataTitleInIndex['weight'][$i]);
     $weightSum = $weightSum + $weightTemp;
     $i++;
 }
-$pepole = count($arrayValue);
-$ageAverage = $ageSum / count($arrayValue);
-$weightAverage = $weightSum / count($arrayValue);
+$pepole = count($usersDataValue);
+$ageAverage = $ageSum / $pepole;
+$weightAverage = $weightSum / $pepole;
 $pepolePrint =  '總共' . $pepole . '人' . PHP_EOL;
 $ageAveragePrint = '平均年紀' . $ageAverage . '歲' . PHP_EOL;
 $weightAveragePrint = '平均公斤' . $weightAverage . '公斤' . PHP_EOL;
 
 
-$columnAnser = [$pepolePrint, $ageAveragePrint, $weightAveragePrint];
+$printAnser = [$pepolePrint, $ageAveragePrint, $weightAveragePrint];
 $file = fopen(__DIR__ . '/2018-08-31-anser.txt', 'w');
-$handle = $file;
-foreach ($columnAnser as $value) {
+$file = $file;
+foreach ($printAnser as $value) {
     $buffer = $value;
-    fwrite($handle, $buffer);
+    fwrite($file, $buffer);
 }
-fclose($handle);
+fclose($file);
